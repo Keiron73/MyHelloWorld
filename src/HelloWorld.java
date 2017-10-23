d7a4623afc5bab7342aad16f48f922bad2c27aa3
Gerald <Gerald@PCE>
2017-10-23 19:51:28 +0200

    DoubleMe Added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,13 @@
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World - edited");
+		
+		HelloWorldTest hwt = new HelloWorldTest();
+		Integer doubleValue = hwt.DoubleMe(3);
+		String value = doubleValue.toString();
+		System.out.println("Double von 3 = ".concat(value));
 	}
 
 }
---
+++ src/HelloWorldTest.java
@@ -0,0 +1,7 @@
+public class HelloWorldTest {
+	public HelloWorldTest() {};
+	
+	public int DoubleMe(int value) {
+		return value * 2;
+	}
+}
